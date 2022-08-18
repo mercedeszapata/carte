694a3926112daf9a300d33bc73a95a89080764f5
mercedes.zapata <mercedeszapatafigueroa@gmail.com>
2022-08-18 19:24:39 -0300

    JSX&Transpiling

---
+++ src/Components/Header/Carrito/Carrito.js
@@ -0,0 +1,9 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './Carrito.css';
+import { rgbToHex } from '@mui/material';
+const Carrito= ()=>{
+    return (
+        <button id="botonCarrito"><ShoppingCartIcon id="carrito" /></button>
+    )
+}
+export default Carrito
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import NavBar from './NavBar/NavBar';
+import Carrito from './Carrito/Carrito';
+import './Header.css';
+
+
+const Header=()=>{
+    return(
+    <header>
+        <div>
+        <img  src="https://i.ibb.co/qjq1LQz/Recurso-14.png" alt="logo" id="logo"></img>
+        <img src="https://i.ibb.co/JnV4bSK/Recurso-1.png" id="nombreMarca"></img>
+        <Carrito />
+        </div>
+       
+        <NavBar />
+       
+    </header>
+        
+    ) 
+        
+}
+
+export default Header
